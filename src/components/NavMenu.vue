5857760165f0005e87b926da41747ad5af6c744d
Felix Gutierrez <fgutie35@southtexascollege.edu>
2024-09-30 18:39:33 -0500

    completed useAuth

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -2,7 +2,6 @@
   <nav class="bg-purple-900 h-20 items-center text-white flex justify-center space-x-6">
     <RouterLink :to="{name: 'Home' }">Home</RouterLink>
     <RouterLink :to="{name: 'Other'}">Other</RouterLink>
-
       <div v-if="isAuthenticated">
         Welcome {{ user.email }}
         <button @click="logout" class="rounded-md bg-red-500 px-4 py-2 mx-2 text-red-100 hover:bg-red-700">LOGOUT</button>
@@ -10,6 +9,8 @@
       <div v-else>
             <RouterLink :to="{name: 'LoginPage'}">Login</RouterLink>
       </div>
+      <label class="text-white flex space-x-6 font-bold">Login (Email: stcadmin@stc.com | Password: 123456!)</label>
+
   </nav>
 </template>
 
