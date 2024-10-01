d91850b8d74c5d6b53420900060e13eb146aa1e8
Felix Gutierrez <fgutie35@southtexascollege.edu>
2024-09-30 21:15:23 -0500

    finished

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -13,17 +13,17 @@ const phone = faker.phone.number({style: 'national'})
 </script>
 
 <template>
-    <!-- {{ fullname }} <br /> -->
+<!-- {{ fullname }} <br /> -->
 
-        <div class="border border-gray-900 rounded-lg shadow bg-gray-300 h-full">
-            <img :src="faker.image.avatar({width: 640, height: 480})" class="rounded-t-xl" />
-            <div class="p-4">
-                <p class="mb-2 text-2x1 font-bold px-3">Name: {{ fullname }}</p>
-                <p class="mb-2 font-normal text-gray-800 px-3">Birthday: {{ birthdate }}</p>
-                <p class="mb-2 font-normal text-gray-800 px-3">Email: {{ email }}</p>
-                <p class="mb-2 font-normal text-gray-800 px-3">About: {{ bio }}</p>
-                <p class="mb-2 font-normal text-gray-800 px-3">Phone: {{ phone }}</p>            
-            </div>
+    <div class="border border-gray-900 rounded-lg shadow bg-gray-300 h-full">
+        <img :src="faker.image.avatar({width: 640, height: 480})" class="rounded-t-xl" />
+        <div class="p-4">
+            <p class="mb-2 text-2x1 font-bold px-3">Name: {{ fullname }}</p>
+            <p class="mb-2 font-normal text-gray-800 px-3">Birthday: {{ birthdate }}</p>
+            <p class="mb-2 font-normal text-gray-800 px-3">Email: {{ email }}</p>
+            <p class="mb-2 font-normal text-gray-800 px-3">About: {{ bio }}</p>
+            <p class="mb-2 font-normal text-gray-800 px-3">Phone: {{ phone }}</p>            
         </div>
-   
+    </div>
+
 </template>
